ec7f88d49bfde4bc5251f7f66d6cf2a8ac090d14
ram <h1610134@gmail.com>
2021-06-16 17:22:17 +0800

    changed name web 16

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,5 +1,11 @@
-function main(): string {
-  return "Hello, world!";
+import {User} from "@/types/user";
+import {getUser, getRepos} from "@/api/api";
+
+async function main(): Promise<string> {
+  const name = "R7774159";
+  const user = await getUser(name);
+  const repos = await getUser(name);
+  return `${user.login} has ${repos.public_repos} repos and ${user.followers} followers`;
 }
 
 export default {
---
+++ src/api/repo.ts
@@ -0,0 +1,4 @@
+export class Repo {
+  stargazers_count!: number;
+
+}
